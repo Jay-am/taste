1298c8af15f2a93fb65555e6966277dce8234263
kaori kohama <kkaori131415@gmail.com>
2020-03-03 18:03:15 +0100

    added method to create profile when user create account

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :profile
+  after_create :create_profile
+
+  def create_profile
+    Profile.create(user: self)
+  end
 end
