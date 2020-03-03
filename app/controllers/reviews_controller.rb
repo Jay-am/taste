188e7d8f6185bf86257971936e3bb81ea25500ab
kaori kohama <kkaori131415@gmail.com>
2020-03-03 14:08:29 +0100

    created review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+    # if params[:restaurant_id].present?
+    #     @restaurants = Restaurant.basic_search(params[:restaurant_id][:name])
+    # else
+    #  @restaurant = Restaurant.all
+    # end
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.special_features = review_params[:special_features] & SPECIAL_FEATURES
+    @review.
+
+    @review.user = current_user
+    @review.restaurant = @restaurant
+
+    if @review.save
+    else
+      render :new
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:restaurant_id, :profile_id, :description, :rating, :meal_rating, :service_rating, :location_rating, :people, special_features: [], occasion: [])
+  end
+
+  private
+  def category
+
+  end
 end
--- config/initializers/constant_loader.rb
+++ config/initializers/constant_loader.rb
@@ -15,3 +15,4 @@ CUISINES = [
   "african",
   "morrocan"
 ]
+
