2df79d1e7e6e18735095caec65cfe277fd880ba0
kaori kohama <kkaori131415@gmail.com>
2020-03-04 17:37:33 +0100

    added edit controller to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,30 @@ class ProfilesController < ApplicationController
   def show
     @profile = Profile.find(params[:id])
   end
+
+  def edit
+    @profile = Profile.find(params[:id])
+    require_owner!(@profile)
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    require_owner!(@profile)
+
+    @profile.update(profile_params)
+    redirect_to profile_path(@profile)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :age, :gender, :address, :photo, :user_id)
+  end
+
+  def require_owner!(profile)
+    if (current_user != profile.user)
+      flash[:notice] = 'You are not authorized to do this.'
+      redirect_to profile_path
+    end
+  end
 end
