ffacb923955cd3bb5bfeef33ed0773378452b3f3
kaori kohama <kkaori131415@gmail.com>
2020-03-05 12:02:10 +0100

    created review index page for each user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,9 @@
 class ReviewsController < ApplicationController
+  def index
+    @profile = Profile.find(params[:profile_id])
+    @reviews = @profile.reviews
+  end
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
