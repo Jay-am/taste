73eebed1cb5f972f7a93a83a3111d5eb66848401
Julia Mackamul <julia.mackamul@gmail.com>
2020-03-05 11:35:25 +0100

    migrated table

--- db/migrate/20200305100348_add_columns_to_restaurant.rb
+++ db/migrate/20200305100348_add_columns_to_restaurant.rb
@@ -1,18 +1,19 @@
 class AddColumnsToRestaurant < ActiveRecord::Migration[5.2]
   def change
-      change_table :restaurants do |t|
-        t.string :styles, array: true
-        t.string :dishes, array: true
-        t.string :food_types, array: true
-        t.string :food_styles, array: true
-        t.string :gastronomies, array: true
-        t.string :drinks, array: true
-        t.string :service, array: true
-        t.string :location, array: true
-        t.string :languages, array: true
-        t.string :open_hours, array: true
-        t.string :distance, array: true
-        t.string :located, array: true
-        t.string :payments, array: true
+    change_table :restaurants do |t|
+      t.string :styles, array: true
+      t.string :dishes, array: true
+      t.string :food_types, array: true
+      t.string :food_styles, array: true
+      t.string :gastronomies, array: true
+      t.string :drinks, array: true
+      t.string :service, array: true
+      t.string :location, array: true
+      t.string :languages, array: true
+      t.string :open_hours, array: true
+      t.string :distance, array: true
+      t.string :located, array: true
+      t.string :payments, array: true
+    end
   end
 end
