8c21cc0358fa168229d64dc7301572d96d045680
Julia Mackamul <julia.mackamul@gmail.com>
2020-03-06 12:45:07 +0100

    changed filter options to be strict and none-strict, refactored code in controller, model and view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -20,23 +20,31 @@ def show
   def index
     @restaurants = Restaurant.all
 
-    @restaurants = @restaurants.by_style(params[:styles])
-    @restaurants = @restaurants.by_cuisine(params[:cuisines])
-    @restaurants = @restaurants.by_dish(params[:dishes])
-    @restaurants = @restaurants.by_food_type(params[:food_types])
-    @restaurants = @restaurants.by_food_style(params[:food_styles])
-    @restaurants = @restaurants.by_gastronomy(params[:gastronomies])
-    @restaurants = @restaurants.by_drink(params[:drinks])
-    @restaurants = @restaurants.by_rating(params[:ratings])
-    @restaurants = @restaurants.by_special_feature(params[:special_features])
-    @restaurants = @restaurants.by_occasion(params[:occasions])
-    @restaurants = @restaurants.by_service(params[:service])
-    @restaurants = @restaurants.by_location(params[:location])
-    @restaurants = @restaurants.by_language(params[:languages])
-    @restaurants = @restaurants.by_open_hour(params[:open_hours])
-    @restaurants = @restaurants.by_distance(params[:distance])
-    @restaurants = @restaurants.by_located(params[:located])
-    @restaurants = @restaurants.by_payment(params[:payments])
+    params[:filters][0].keys.each do |key|
+      @restaurants = @restaurants.send("by_#{key}", params[:filters][0][key])
+    end
+
+    params[:filters][0].keys.each do |key|
+      @restaurants = @restaurants.send("with_#{key}", params[:filters][0][key])
+    end
+
+    # @restaurants = @restaurants.by_style(params[:styles])
+    # @restaurants = @restaurants.by_cuisine(params[:cuisines])
+    # @restaurants = @restaurants.by_dish(params[:dishes])
+    # @restaurants = @restaurants.by_food_type(params[:food_types])
+    # @restaurants = @restaurants.by_food_style(params[:food_styles])
+    # @restaurants = @restaurants.by_gastronomy(params[:gastronomies])
+    # @restaurants = @restaurants.by_drink(params[:drinks])
+    # @restaurants = @restaurants.by_rating(params[:ratings])
+    # @restaurants = @restaurants.by_special_feature(params[:special_features])
+    # @restaurants = @restaurants.by_occasion(params[:occasions])
+    # @restaurants = @restaurants.by_service(params[:service])
+    # @restaurants = @restaurants.by_location(params[:location])
+    # @restaurants = @restaurants.by_language(params[:languages])
+    # @restaurants = @restaurants.by_open_hour(params[:open_hours])
+    # @restaurants = @restaurants.by_distance(params[:distance])
+    # @restaurants = @restaurants.by_located(params[:located])
+    # @restaurants = @restaurants.by_payment(params[:payments])
 
   end
 
