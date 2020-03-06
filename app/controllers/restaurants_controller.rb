bf2331f117b2763e6f2701a49dcc0786bddf094a
Abel van Esseveldt <abelvess@gmail.com>
2020-03-06 14:59:39 +0100

    map working for user current location

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,29 +14,33 @@ def show
   }
   end
 
+  def map
+    @markers = { lat: params[:lat], lng: params[:lng] }
+  end
+
   def filter
   end
 
   def index
     @restaurants = Restaurant.all
 
-    @restaurants = @restaurants.by_style(params[:styles])
-    @restaurants = @restaurants.by_cuisine(params[:cuisines])
-    @restaurants = @restaurants.by_dish(params[:dishes])
-    @restaurants = @restaurants.by_food_type(params[:food_types])
-    @restaurants = @restaurants.by_food_style(params[:food_styles])
-    @restaurants = @restaurants.by_gastronomy(params[:gastronomies])
-    @restaurants = @restaurants.by_drink(params[:drinks])
-    @restaurants = @restaurants.by_rating(params[:ratings])
-    @restaurants = @restaurants.by_special_feature(params[:special_features])
-    @restaurants = @restaurants.by_occasion(params[:occasions])
-    @restaurants = @restaurants.by_service(params[:service])
-    @restaurants = @restaurants.by_location(params[:location])
-    @restaurants = @restaurants.by_language(params[:languages])
-    @restaurants = @restaurants.by_open_hour(params[:open_hours])
-    @restaurants = @restaurants.by_distance(params[:distance])
-    @restaurants = @restaurants.by_located(params[:located])
-    @restaurants = @restaurants.by_payment(params[:payments])
+    # @restaurants = @restaurants.by_style(params[:styles])
+    # @restaurants = @restaurants.by_cuisine(params[:cuisines])
+    # @restaurants = @restaurants.by_dish(params[:dishes])
+    # @restaurants = @restaurants.by_food_type(params[:food_types])
+    # @restaurants = @restaurants.by_food_style(params[:food_styles])
+    # @restaurants = @restaurants.by_gastronomy(params[:gastronomies])
+    # @restaurants = @restaurants.by_drink(params[:drinks])
+    # @restaurants = @restaurants.by_rating(params[:ratings])
+    # @restaurants = @restaurants.by_special_feature(params[:special_features])
+    # @restaurants = @restaurants.by_occasion(params[:occasions])
+    # @restaurants = @restaurants.by_service(params[:service])
+    # @restaurants = @restaurants.by_location(params[:location])
+    # @restaurants = @restaurants.by_language(params[:languages])
+    # @restaurants = @restaurants.by_open_hour(params[:open_hours])
+    # @restaurants = @restaurants.by_distance(params[:distance])
+    # @restaurants = @restaurants.by_located(params[:located])
+    # @restaurants = @restaurants.by_payment(params[:payments])
 
   end
 
