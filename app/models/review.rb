b92920f1dcdf9bd2b0ad0a6e7b27b0e2110afd52
Julia Mackamul <julia.mackamul@gmail.com>
2020-03-06 16:41:03 +0100

    added new pages bookmark_show and review_show

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :profile
   belongs_to :restaurant
+
+  act_as_bookmarkee
+
   validates :meal_rating, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :service_rating, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :location_rating, inclusion: { in: [1, 2, 3, 4, 5] }
