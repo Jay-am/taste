2706c591d40f845595e7be01fb48e295073b3994
kaori kohama <kkaori131415@gmail.com>
2020-03-11 19:37:41 +0100

    added users and reviews to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,134 +1,182 @@
 require 'open-uri'
 require 'json'
-
 puts "Cleaning up data"
 Restaurant.destroy_all
 User.destroy_all
 Profile.destroy_all
 Review.destroy_all
+USER_INFORMATION = [
+  {
+    email: 'nature_boy@gmail.com',
+    password: 'testing',
+    profile: {
+      first_name: "David",
+      last_name: "Thoreau",
+      information: "Just love food. There's nothing that makes me happier."
+    },
+    {
+    email: 'k.johnson@gmail.com',
+    password: 'testing',
+    profile: {
+      first_name: "Kamal",
+      last_name: "Johnson",
+      information: "Food just amazes me. I can spend all day thinking of it."
+    },
+    {
+    email: 'rayan@gmail.com',
+    password: 'testing',
+    profile: {
+      first_name: "Rayan",
+      last_name: "laStrada",
+      information: "Passionate about Food."
+    },
+    {
+    email: 'emma@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Emma",
+      last_name: "Smith",
+      information: "Just love food. There's nothing that makes me happier."
+    },
+    {
+    email: 'olivia@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Olivia",
+      last_name: "Williams",
+      information: "Food just amazes me. I can spend all day thinking of it."
+    },
+    {
+    email: 'ava@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Ava",
+      last_name: "Moore",
+      information: "Passionate about Food."
+    },
+    {
+    email: 'sophia@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Sophia",
+      last_name: "Scott",
+      information: "I could go to a restaurant every day. No better 3 times a day!"
+    },
+    {
+    email: 'charlotte@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Charlotte",
+      last_name: "Hill",
+      information: "I am a Food Photographer and Columnist of Ohhhhhhh that foooooood! DM me for fancy Restaurant Tips."
+    },
+    {
+    email: 'mia@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Mia",
+      last_name: "Davis",
+      information: "Never met a calorie I didn't like!"
+    },
+    {
+    email: 'amelia@email.com',
+    password: 'testing',
+    profile: {
+      first_name: "Amelia",
+      last_name: "Miller",
+      information: "Food is our Common Ground. A universal Experience."
+    }
+  }
+]
+
+REVIEWS = [
+  'Amazing restaurant! Had a beautiful dinner night!!',
+  'Amazing Food! Highly recommended!!',
+  'I love this restaurant! I had a great experience',
+  'Great Restaurant! Love it!',
+  'Beautiful restaurant!',
+  'Good food! good service!!',
+  'Amazing restaurant!',
+  'Bloodly lovely food.',
+  'Visited this restaurant with my gf! She loves it.',
+  'Had a amazing couple dinner.'
+  'Good food! Good service! Good location!'
+]
 
 puts "Creating new restaurants"
 restaurant1 = Restaurant.new(name: "The French Connection", address: "Hammarbystraat 5, Amsterdam", description: "Nice french cuisine, lovely staff, amazing atmosphere.", cuisine: ["French"], occasion: ["Candle Lit"], special_features: ["Open Fire"])
 restaurant2 = Restaurant.new(name: "Choux", address: "Singel 460, Amsterdam", description: "Vegetarian, progressive, quirky location, great food.", cuisine: ["French"], occasion: ["Couple"], special_features: ["Vegetarian"])
 restaurant3 = Restaurant.new(name: "Burger King", address: "Spadinalaan 50, Amsterdam", description: "Classic american style fastfood.", cuisine: ["American"], occasion: ["Group"], special_features: ["Vegetarian"])
-
 restaurant1.save!
 restaurant2.save!
 restaurant3.save!
-
 puts "Making users"
-user1 = User.new(email: "nature_boy@gmail.com", password: "testing")
-user2 = User.new(email: "k.johnson@gmail.com", password: "testing")
-user3 = User.new(email: "rayan@gmail.com", password: "testing")
-user4 = User.new(email: "emma@email.com", password: "123456")
-user5 = User.new(email: "olivia@email.com", password: "123456")
-user6 = User.new(email: "ava@email.com", password: "123456")
-user7 = User.new(email: "sophia@email.com", password: "123456")
-user8 = User.new(email: "charlotte@email.com", password: "123456")
-user9 = User.new(email: "mia@email.com", password: "123456")
-user10 = User.new(email: "amelia@email.com", password: "123456")
-user11 = User.new(email: "grace@email.com", password: "123456")
-user12 = User.new(email: "chloe@email.com", password: "123456")
-user13 = User.new(email: "zoey@email.com", password: "123456")
-user14 = User.new(email: "nora@email.com", password: "123456")
-user15 = User.new(email: "hannah@email.com", password: "123456")
-user16 = User.new(email: "lucy@email.com", password: "123456")
-user17 = User.new(email: "claire@email.com", password: "123456")
-user18 = User.new(email: "anna@email.com", password: "123456")
-user19 = User.new(email: "liam@email.com", password: "123456")
-user20 = User.new(email: "noah@email.com", password: "123456")
-user21 = User.new(email: "william@email.com", password: "123456")
-user22 = User.new(email: "james@email.com", password: "123456")
-user23 = User.new(email: "oliver@email.com", password: "123456")
 
-user1.save!
-user2.save!
-user3.save!
-user4.save!
-user5.save!
-user6.save!
-user7.save!
-user8.save!
-user9.save!
-user10.save!
-user11.save!
-user12.save!
-user13.save!
-user14.save!
-user15.save!
-user16.save!
-user17.save!
-user18.save!
-user19.save!
-user20.save!
-user21.save!
-user22.save!
-user23.save!
 
-puts "Assigning users to profiles"
-profile1 = Profile.new(user: user1, first_name: "David", last_name: "Thoreau")
-profile2 = Profile.new(user: user2, first_name: "Kamal", last_name: "Johnson")
-profile3 = Profile.new(user: user3, first_name: "Rayan", last_name: "laStrada")
-profile4 = Profile.new(user: user4, first_name: "Emma", last_name: "Smith", information: "Just love food. There's nothing that makes me happier.")
-profile5 = Profile.new(user: user5, first_name: "Olivia", last_name: "Williams", information: "Food just amazes me. I can spend all day thinking of it.")
-profile6 = Profile.new(user: user6, first_name: "Ava", last_name: "Moore", information: "Passionate about Food.")
-profile7 = Profile.new(user: user7, first_name: "Sophia", last_name: "Scott", information: "I could go to a restaurant every day. No better 3 times a day!")
-profile8 = Profile.new(user: user8, first_name: "Charlotte", last_name: "Hill", information: "I am a Food Photographer and Columnist of Ohhhhhhh that foooooood! DM me for fancy Restaurant Tips.")
-profile9 = Profile.new(user: user9, first_name: "Mia", last_name: "Davis", information: "Never met a calorie I didn't like!")
-profile10 = Profile.new(user: user10, first_name: "Amelia", last_name: "Miller", information: "But First.....Food!")
-profile11 = Profile.new(user: user11, first_name: "Grace", last_name: "Wilson", information: "Foodie and Food Photographer.")
-profile12 = Profile.new(user: user12, first_name: "Chloe", last_name: "Parker", information: "Yes I can eat all day long :)")
-profile13 = Profile.new(user: user13, first_name: "Zoey", last_name: "Harris", information: "Passionate about Cupcakes. Know all the best places to get them!")
-profile14 = Profile.new(user: user14, first_name: "Nora", last_name: "Jones", information: "Food is just amazing. It not only tastes incredible, it also connects people.")
-profile15 = Profile.new(user: user15, first_name: "Hannah", last_name: "Peterson", information: "Everything is better with Chocolate!")
-profile16 = Profile.new(user: user16, first_name: "Lucy", last_name: "Morgan", information: "Food? Always!")
-profile17 = Profile.new(user: user17, first_name: "Claire", last_name: "Howard", information: "Food is our Common Ground. A universal Experience.")
-profile18 = Profile.new(user: user18, first_name: "Anna", last_name: "Taylor", information: "Ohhhhhhh that foooooood!")
-profile19 = Profile.new(user: user19, first_name: "Liam", last_name: "Johnson", information: "All you need is.....Steak!")
-profile20 = Profile.new(user: user20, first_name: "Noah", last_name: "Clark", information: "Give me those burgers!!!!")
-profile21 = Profile.new(user: user21, first_name: "William", last_name: "Torres", information: "Vegetarian on Rehab!")
-profile22 = Profile.new(user: user22, first_name: "James", last_name: "Brown", information: "Meat addicted. Not even lying!")
-profile23 = Profile.new(user: user23, first_name: "Oliver", last_name: "Carter", information: "What I love about food? Everything!")
+USER_INFORMATION.each do |user_data|
+  user = User.create email: user_data[:email], password: user_data[:password]
+  profile = Profile.create user: user, first_name: user_data[:profile][:first_name], last_name: user_data[:profile][:last_name], first_name: user_data[:profile][:description]
 
-profile1.save!
-profile2.save!
-profile3.save!
-profile4.save!
-profile5.save!
-profile6.save!
-profile7.save!
-profile8.save!
-profile9.save!
-profile10.save!
-profile11.save!
-profile12.save!
-profile13.save!
-profile14.save!
-profile15.save!
-profile16.save!
-profile17.save!
-profile18.save!
-profile19.save!
-profile20.save!
-profile21.save!
-profile22.save!
-profile23.save!
+  REVIEWS.each do |desc|
+    Review.create(
+      description: desc,
+      rating: rand(3..5),
+      meal_rating: rand(3..4),
+      service_rating: rand(3..5),
+      location_rating: rand(3..5),
+      cuisine: [CUISINES.sample],
+      occasion: [OCCASIONS.sample],
+      special_features: [SPECIAL_FEATURES.sample],
+      local: true
+    )
+  end
+end
 
+
+# user11 = User.new(email: "grace@email.com", password: "123456")
+# user12 = User.new(email: "chloe@email.com", password: "123456")
+# user13 = User.new(email: "zoey@email.com", password: "123456")
+# user14 = User.new(email: "nora@email.com", password: "123456")
+# user15 = User.new(email: "hannah@email.com", password: "123456")
+# user16 = User.new(email: "lucy@email.com", password: "123456")
+# user17 = User.new(email: "claire@email.com", password: "123456")
+# user18 = User.new(email: "anna@email.com", password: "123456")
+# user19 = User.new(email: "liam@email.com", password: "123456")
+# user20 = User.new(email: "noah@email.com", password: "123456")
+# user21 = User.new(email: "william@email.com", password: "123456")
+# user22 = User.new(email: "james@email.com", password: "123456")
+# user23 = User.new(email: "oliver@email.com", password: "123456")
+# puts "Assigning users to profiles"
+
+
+
+
+# profile10 = Profile.new(user: user10, first_name: "Amelia", last_name: "Miller", information: "But First.....Food!")
+# profile11 = Profile.new(user: user11, first_name: "Grace", last_name: "Wilson", information: "Foodie and Food Photographer.")
+# profile12 = Profile.new(user: user12, first_name: "Chloe", last_name: "Parker", information: "Yes I can eat all day long :)")
+# profile13 = Profile.new(user: user13, first_name: "Zoey", last_name: "Harris", information: "Passionate about Cupcakes. Know all the best places to get them!")
+# profile14 = Profile.new(user: user14, first_name: "Nora", last_name: "Jones", information: "Food is just amazing. It not only tastes incredible, it also connects people.")
+# profile15 = Profile.new(user: user15, first_name: "Hannah", last_name: "Peterson", information: "Everything is better with Chocolate!")
+# profile16 = Profile.new(user: user16, first_name: "Lucy", last_name: "Morgan", information: "Food? Always!")
+# profile17 = Profile.new(user: user17, first_name: "Claire", last_name: "Howard", information: "Food is our Common Ground. A universal Experience.")
+# profile18 = Profile.new(user: user18, first_name: "Anna", last_name: "Taylor", information: "Ohhhhhhh that foooooood!")
+# profile19 = Profile.new(user: user19, first_name: "Liam", last_name: "Johnson", information: "All you need is.....Steak!")
+# profile20 = Profile.new(user: user20, first_name: "Noah", last_name: "Clark", information: "Give me those burgers!!!!")
+# profile21 = Profile.new(user: user21, first_name: "William", last_name: "Torres", information: "Vegetarian on Rehab!")
+# profile22 = Profile.new(user: user22, first_name: "James", last_name: "Brown", information: "Meat addicted. Not even lying!")
+# profile23 = Profile.new(user: user23, first_name: "Oliver", last_name: "Carter", information: "What I love about food? Everything!")
 puts "Making reviews"
 review1 = Review.new(description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quae sunt excepturi magnam porro maxime earum, deleniti magni. Cupiditate nobis ducimus, eius quaerat! Perferendis necessitatibus rem inventore reiciendis amet veniam!", rating: 3, meal_rating: 5, service_rating: 2, location_rating: 2, cuisine: ["french"], occasion: ["candle light"], special_features: ["fireplace"], restaurant: restaurant1, profile: profile1, local: false)
 review2 = Review.new(description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quae sunt excepturi magnam porro maxime earum, deleniti magni. Cupiditate nobis ducimus, eius quaerat! Perferendis necessitatibus rem inventore reiciendis amet veniam!", rating: 3, meal_rating: 5, service_rating: 2, location_rating: 2, cuisine: ["french"], occasion: ["candle light"], special_features: ["fireplace"], restaurant: restaurant2, profile: profile2, local: true)
 review3 = Review.new(description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quae sunt excepturi magnam porro maxime earum, deleniti magni. Cupiditate nobis ducimus, eius quaerat! Perferendis necessitatibus rem inventore reiciendis amet veniam!", rating: 3, meal_rating: 5, service_rating: 2, location_rating: 2, cuisine: ["french"], occasion: ["candle light"], special_features: ["fireplace"], restaurant: restaurant3, profile: profile3, local: true)
 review4 = Review.new(description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quae sunt excepturi magnam porro maxime earum, deleniti magni. Cupiditate nobis ducimus, eius quaerat! Perferendis necessitatibus rem inventore reiciendis amet veniam!", rating: 3, meal_rating: 5, service_rating: 2, location_rating: 2, cuisine: ["french"], occasion: ["candle light"], special_features: ["fireplace"], restaurant: restaurant1, profile: profile2, local: false)
 review5 = Review.new(description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quae sunt excepturi magnam porro maxime earum, deleniti magni. Cupiditate nobis ducimus, eius quaerat! Perferendis necessitatibus rem inventore reiciendis amet veniam!", rating: 3, meal_rating: 5, service_rating: 2, location_rating: 2, cuisine: ["french"], occasion: ["candle light"], special_features: ["fireplace"], restaurant: restaurant1, profile: profile3, local: false)
-
 review1.save!
 review2.save!
 review3.save!
 review4.save!
 review5.save!
-
 puts "How many JSON's are you creating? 'All of them'"
-
 url = 'http://open.data.amsterdam.nl/EtenDrinken.json'
 amsterdam = open(url).read
 json = JSON.parse(amsterdam)
@@ -138,10 +186,6 @@ json[1..-1].each do |data| # [1..-1] ignores first dummy element
     address: data['location']['adress']
   )
 end
-
-
-
-
 
 
 
